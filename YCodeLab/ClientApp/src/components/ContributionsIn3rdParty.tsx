63c2030fb81d845101f8f0f2fcfa820d93107903
Yas <yasu.gemini@gmail.com>
2020-04-23 01:32:27 +1200

    updated content in My Projects page

--- YCodeLab/ClientApp/src/components/ContributionsIn3rdParty.tsx
+++ YCodeLab/ClientApp/src/components/ContributionsIn3rdParty.tsx
@@ -11,17 +11,18 @@ import linkInjector from '../utilities/linkInjector';
 interface Props {
 }
 interface State {
-    thirdPartyLib: { items: ThirdPartyLibItem[] }
+  thirdPartyLib: { items: ThirdPartyLibItem[] }
 }
 
 interface ThirdPartyLibItem {
-    libName: string,
-    logo: string,
-    actionType: ('bug fix'|'enhancement'|'improvement'|'doc'),
-    status: string,
-    title: string,
-    description: string[],
-    links: { [key:string]: string },
+  id: string,
+  libName: string,
+  logo: string,
+  actionType: ('bug fix'|'enhancement'|'improvement'|'doc'),
+  status: string,
+  title: string,
+  description: string[],
+  links: { [key:string]: string },
 }
 
 export default class ThirdPartyLibDevs extends React.PureComponent<Props, State> {
@@ -29,107 +30,113 @@ export default class ThirdPartyLibDevs extends React.PureComponent<Props, State>
     super(props);
 
     this.state = {
-        thirdPartyLib: {
-            items: [
-                {
-                    libName: 'dotnet/efcore',
-                    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/64px-.NET_Core_Logo.svg.png',
-                    actionType: 'bug fix',
-                    status: 'released in .NET Core 3.0.0-preview6',
-                    title: 'scaffold-DbContext not adding schema parameter to modelBuilder.HasSequence',
-                    description: [
-                        'DB context scaffold did not generate modelBuilder.HasSequence method call with a schema parameter in DbContext\'s OnModelCreating method.',
-                        'I fixed the condition in CSharpDbContextGenerator.GenerateSequence to correctly append schema parameter to sequence creation code when the sequence\'s schema is provided. I raised the issue in #15513 and fixed in #15514. It\'s been released in .NET Core 3.0.0-preview6',
-                    ],
-                    links: {
-                        '#15513': 'https://github.com/dotnet/efcore/issues/15513',
-                        '#15514': 'https://github.com/dotnet/efcore/pull/15514',
-                        '.NET Core 3.0.0-preview6': 'https://github.com/dotnet/efcore/releases/tag/release%2F3.0',
-                    },
-                },
-                {
-                    libName: 'npgsql/npgsql',
-                    logo: 'https://avatars2.githubusercontent.com/u/5766497?s=200&v=4',
-                    actionType: 'enhancement',
-                    status: 'open',
-                    title: 'support connect URI',
-                    description: [
-                        'When I tried to host my ASP.Net Core app on AppHarbor, I found that ElephantSQL add-on provides a connection URI for PostgreSQL database connection. It was a problem because Npgsql does not support connection URI.',
-                        'It seems to be quite common among PaaS services that integrated PostgreSQL services provide connection URI instead of connection string, so it was surprising for me to find it not acceptable in Npgsql. The issue has already been raised in GitHub since August 2018 in #2090, but it has not been solved yet. Although one contributor committed a pull request against the issue in the past in #2512, it was not a complete implementation.',
-                        'After reading through the comments and changes in #2090 and #2512, I noticed that this feature requires much more effort and that the approach in #3512 does not work in some cases. Besides, when I found the pull request, it looked not active. The comments on its code review had not been answered for more than three months. Therefore, I decided to solve it myself and provided the solution in a separate pull request in #2733.',
-                    ],
-                    links: {
-                        '#2733': 'https://github.com/npgsql/npgsql/pull/2733',
-                        '#2090': 'https://github.com/npgsql/npgsql/issues/2090',
-                        'AppHarbor': 'https://appharbor.com/',
-                        '#2512': 'https://github.com/npgsql/npgsql/pull/2512',
-                    },
-                },
-                {
-                    libName: 'danielfarrell/bootstrap-combobox',
-                    logo: 'https://v4-alpha.getbootstrap.com/assets/brand/bootstrap-solid.svg',
-                    actionType: 'enhancement',
-                    status: 'released in 1.2.0',
-                    title: 'support Bootstrap 4',
-                    description: [
-                        'When I used bootstrap-combobox with Bootstrap 4, the combo-box looked not good, where the caret icon appeared outside the input. Besides, there was a malfunction when Popper.js is referenced.',
-                        'The layout broke because Glyphicons was taken off in Bootstrap 4. It needed to get rid of the dependency on Glyphicons and to be able to work with any other font.',
-                        'The combo-box behaved a little differently when a web page also refers to Popper.js. Some Bootstrap 4 components rely on the library, so the bootstrap-combobox should work the same with or without Popper.js.',
-                        'I fixed those two in #263, and it was released in bootstrap-combobox 1.2.0.',
-                    ],
-                    links: {
-                        '#263': 'https://github.com/danielfarrell/bootstrap-combobox/pull/263',
-                        'bootstrap-combobox 1.2.0': 'https://github.com/danielfarrell/bootstrap-combobox/releases/tag/1.2.0',
-                    },
-                },
-                {
-                    libName: 'zzzprojects/html-agility-pack',
-                    logo: 'https://z2c2b4z9.stackpathcdn.com/images/logo256X256.png',
-                    actionType: 'improvement',
-                    status: 'open',
-                    title: 'Handle not supported content encoding',
-                    description: [
-                        'Html Agility Pack (HAP) is a library I use in this web site and my web application, HappyFL. This library helps seeking data from HTML documents provided on the web. In this web site, I use it to get some information from Code Project web site in Technical Writings page because its REST API does not provide enough information I need.',
-                        'While using HAP in HappyFL, I found that the library throws an exception when loading a page from a specific recipe web site. Checking the response header, I noticed that the web site responds with a header which breaches HTTP protocol, and HAP failed to solve the encoding scheme as a result.',
-                        'Fortunately, HAP can overwrite the encoding scheme to read responses, and I tried it. However, the feature did not help in this case. I read the code in HAP, and found that it is possible to make the encoding overwrite work even in such case. So, I modified the code and committed it with some more test code in pull request #327.',
-                    ],
-                    links: {
-                        'HappyFL': 'http://happyfl.apphb.com/',
-                        'Technical Writings page': 'https://y-code-lab.herokuapp.com/tech-writings',
-                        '#326': 'https://github.com/zzzprojects/html-agility-pack/issues/326',
-                        '#327': 'https://github.com/zzzprojects/html-agility-pack/pull/327',
-                    },
-                },
-                {
-                    libName: 'reactstrap/reactstrap',
-                    logo: 'https://reactstrap.github.io/assets/logo.png',
-                    actionType: 'doc',
-                    status: 'released',
-                    title: 'correct broken links',
-                    description: [
-                        'When I was reading the documentation, I noticed a broken link and an inconsistent command between reactstrap doc and create-react-app doc. Therefore, I committed some updates in #1494.',
-                    ],
-                    links: {
-                        'reactstrap doc': 'https://reactstrap.github.io/',
-                        '#1494': 'https://github.com/reactstrap/reactstrap/pull/1494',
-                    },
-                },
-                {
-                    libName: 'rabbitmq/rabbitmq-website',
-                    logo: 'https://cdn.worldvectorlogo.com/logos/rabbitmq.svg',
-                    actionType: 'doc',
-                    status: 'released',
-                    title: 'correct directory paths in instruction for macOS',
-                    description: [
-                        'When I was playing with rabbitmq in my macOS, I noticed a couple of paths in the instruction for macOS were different from what I could see. So, I corrected it in #731.',
-                    ],
-                    links: {
-                        'the instruction for macOS': 'https://www.rabbitmq.com/install-homebrew.html',
-                        '#731': 'https://github.com/rabbitmq/rabbitmq-website/pull/731',
-                    },
-                },
-            ]
-        }
+      thirdPartyLib: {
+        items: [
+          {
+            id: 'ef-core-bug-fix-0',
+            libName: 'dotnet/efcore',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/64px-.NET_Core_Logo.svg.png',
+            actionType: 'bug fix',
+            status: 'released in .NET Core 3.0.0-preview6',
+            title: 'scaffold-DbContext not adding schema parameter to modelBuilder.HasSequence',
+            description: [
+              'DB context scaffold did not generate modelBuilder.HasSequence method call with a schema parameter in DbContext\'s OnModelCreating method.',
+              'I fixed the condition in CSharpDbContextGenerator.GenerateSequence to correctly append schema parameter to sequence creation code when the sequence\'s schema is provided. I raised the issue in #15513 and fixed in #15514. It\'s been released in .NET Core 3.0.0-preview6',
+            ],
+            links: {
+              '#15513': 'https://github.com/dotnet/efcore/issues/15513',
+              '#15514': 'https://github.com/dotnet/efcore/pull/15514',
+              '.NET Core 3.0.0-preview6': 'https://github.com/dotnet/efcore/releases/tag/release%2F3.0',
+            },
+          },
+          {
+            id: 'npgsql-enhancement-0',
+            libName: 'npgsql/npgsql',
+            logo: 'https://avatars2.githubusercontent.com/u/5766497?s=200&v=4',
+            actionType: 'enhancement',
+            status: 'open',
+            title: 'support connect URI',
+            description: [
+              'When I tried to host my ASP.Net Core app on AppHarbor, I found that ElephantSQL add-on provides a connection URI for PostgreSQL database connection. It was a problem because Npgsql does not support connection URI.',
+              'It seems to be quite common among PaaS services that integrated PostgreSQL services provide connection URI instead of connection string, so it was surprising for me to find it not acceptable in Npgsql. The issue has already been raised in GitHub since August 2018 in #2090, but it has not been solved yet. Although one contributor committed a pull request against the issue in the past in #2512, it was not a complete implementation.',
+              'After reading through the comments and changes in #2090 and #2512, I noticed that this feature requires much more effort and that the approach in #3512 does not work in some cases. Besides, when I found the pull request, it looked not active. The comments on its code review had not been answered for more than three months. Therefore, I decided to solve it myself and provided the solution in a separate pull request in #2733.',
+            ],
+            links: {
+              '#2733': 'https://github.com/npgsql/npgsql/pull/2733',
+              '#2090': 'https://github.com/npgsql/npgsql/issues/2090',
+              'AppHarbor': 'https://appharbor.com/',
+              '#2512': 'https://github.com/npgsql/npgsql/pull/2512',
+            },
+          },
+          {
+            id: 'npgsql-enhancement-0',
+            libName: 'danielfarrell/bootstrap-combobox',
+            logo: 'https://v4-alpha.getbootstrap.com/assets/brand/bootstrap-solid.svg',
+            actionType: 'enhancement',
+            status: 'released in 1.2.0',
+            title: 'support Bootstrap 4',
+            description: [
+              'When I used bootstrap-combobox with Bootstrap 4, the combo-box looked not good, where the caret icon appeared outside the input. Besides, there was a malfunction when Popper.js is referenced.',
+              'The layout broke because Glyphicons was taken off in Bootstrap 4. It needed to get rid of the dependency on Glyphicons and to be able to work with any other font.',
+              'The combo-box behaved a little differently when a web page also refers to Popper.js. Some Bootstrap 4 components rely on the library, so the bootstrap-combobox should work the same with or without Popper.js.',
+              'I fixed those two in #263, and it was released in bootstrap-combobox 1.2.0.',
+            ],
+            links: {
+              '#263': 'https://github.com/danielfarrell/bootstrap-combobox/pull/263',
+              'bootstrap-combobox 1.2.0': 'https://github.com/danielfarrell/bootstrap-combobox/releases/tag/1.2.0',
+            },
+          },
+          {
+            id: 'hap-improvement-0',
+            libName: 'zzzprojects/html-agility-pack',
+            logo: 'https://z2c2b4z9.stackpathcdn.com/images/logo256X256.png',
+            actionType: 'improvement',
+            status: 'open',
+            title: 'Handle not supported content encoding',
+            description: [
+              'Html Agility Pack (HAP) is a library I use in this web site and my web application, HappyFL. This library helps seeking data from HTML documents provided on the web. In this web site, I use it to get some information from Code Project web site in Technical Writings page because its REST API does not provide enough information I need.',
+              'While using HAP in HappyFL, I found that the library throws an exception when loading a page from a specific recipe web site. Checking the response header, I noticed that the web site responds with a header which breaches HTTP protocol, and HAP failed to solve the encoding scheme as a result.',
+              'Fortunately, HAP can overwrite the encoding scheme to read responses, and I tried it. However, the feature did not help in this case. I read the code in HAP, and found that it is possible to make the encoding overwrite work even in such case. So, I modified the code and committed it with some more test code in pull request #327.',
+            ],
+            links: {
+              'HappyFL': 'http://happyfl.apphb.com/',
+              'Technical Writings page': 'https://y-code-lab.herokuapp.com/tech-writings',
+              '#326': 'https://github.com/zzzprojects/html-agility-pack/issues/326',
+              '#327': 'https://github.com/zzzprojects/html-agility-pack/pull/327',
+            },
+          },
+          {
+            id: 'reactstrap-doc-0',
+            libName: 'reactstrap/reactstrap',
+            logo: 'https://reactstrap.github.io/assets/logo.png',
+            actionType: 'doc',
+            status: 'released',
+            title: 'correct broken links',
+            description: [
+              'When I was reading the documentation, I noticed a broken link and an inconsistent command between reactstrap doc and create-react-app doc. Therefore, I committed some updates in #1494.',
+            ],
+            links: {
+              'reactstrap doc': 'https://reactstrap.github.io/',
+              '#1494': 'https://github.com/reactstrap/reactstrap/pull/1494',
+            },
+          },
+          {
+            id: 'rabbitmq-doc-0',
+            libName: 'rabbitmq/rabbitmq-website',
+            logo: 'https://cdn.worldvectorlogo.com/logos/rabbitmq.svg',
+            actionType: 'doc',
+            status: 'released',
+            title: 'correct directory paths in instruction for macOS',
+            description: [
+              'When I was playing with rabbitmq in my macOS, I noticed a couple of paths in the instruction for macOS were different from what I could see. So, I corrected it in #731.',
+            ],
+            links: {
+              'the instruction for macOS': 'https://www.rabbitmq.com/install-homebrew.html',
+              '#731': 'https://github.com/rabbitmq/rabbitmq-website/pull/731',
+            },
+          },
+        ]
+      }
     };
   }
 
@@ -140,7 +147,7 @@ export default class ThirdPartyLibDevs extends React.PureComponent<Props, State>
   render() {
     return (
       <div className="page-contributions-in-3rd-party">
-        <h1>Contributions in Third Party Software</h1>
+        <h1>Contributions in Third-Party Software</h1>
         {/* <object type="image/svg+xml" data="/tpl-frame.svg"/> */}
         {
           (() => {
--- YCodeLab/ClientApp/src/components/MyProjects.tsx
+++ YCodeLab/ClientApp/src/components/MyProjects.tsx
@@ -15,6 +15,32 @@ export default class MyProjects extends React.PureComponent<{
         <h2>Web Application Developments</h2>
 
         <h3>
+          <div style={{ marginRight: "10px", backgroundImage: "url(/favicon-32.png)", backgroundRepeat: "no-repeat", display: "inline", width: "32px", height: "32px"}}>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </div>
+          Y-Code Lab
+          <a href="https://github.com/y-code/y-code-lab" target="_blank"><img src="/GitHub-Mark-64px.png" style={{ width: "26px", marginLeft: "10px" }}/></a>
+        </h3>
+        <Container>
+          <Row>
+            <Col lg={12}>
+              <p>
+                Y-Code Lab is my profile website here. I'm using ASP.Net Core SPA with React/Redux and Entity Framework with PostgreSQL for this web application development. I dockerize it and am hosting the docker container at Heroku.
+              </p>
+              <p>
+                A service is running in the backend to cache updates from <a href="https://api.codeproject.com/">Code Project API</a> in terms of my articles. This API is still in beta release, and it turned to be very unstable where requests sometimes fail. Therefore, I created the caching service to provide my articles information on <Link to="/tech-writings">Technical Writings page</Link> in a stable.
+              </p>
+              <p>
+                Entity Framework is for saving the message on <Link to="/contact-me">Contact ME page</Link>.
+              </p>
+              <p>
+                The source code of this web application is in public in <a href="https://github.com/y-code/y-code-lab" target="_blank">y-code/y-code-lab repository at GitHub</a>. Please feel free to look over it.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+
+        <h3>
           <div style={{ marginRight: "10px", backgroundImage: "url(https://happyfl.apphb.com/favicon.png)", backgroundRepeat: "no-repeat", display: "inline", width: "32px", height: "32px"}}>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           </div>
@@ -25,10 +51,13 @@ export default class MyProjects extends React.PureComponent<{
           <Row>
             <Col lg={12} xl={6}>
               <p>
-                This is my personal development. This web application is a daily food management system, which integrates meal planning, shopping list generation, and recipe management. The development is still at the beginning. What you can do so far is to parse recipe webpages, to save the parsed recipe information, and to view the saved recipes (dishes).
+                HappyFL is a web application I'm personally developing. I'm using ASP.Net Core SPA with Angular and Entity Framework with PostgreSQL for this web application development.
               </p>
               <p>
-                This web application is created using ASP.Net Core, Entity Framework with PostgreSQL, and Angular.
+                This web application is a daily food management system, which integrates meal planning, shopping list generation, and recipe management. The development is still at the beginning, and what you can do so far is to parse recipe webpages, to save the parsed recipe information, and to view the saved recipes (dishes).
+              </p>
+              <p>
+                By the way, the enhancement in Npgsql that I'm pull-requesting is used in this application to host it at App Harbor. Please find the details of the enhancement in <Link to="/tech-writings">Contributions in Third-Party Software page here</Link> or <a href="https://github.com/npgsql/npgsql/pull/2733">my pull request at GitHub</a>.
               </p>
             </Col>
             <Col lg={12} xl={6} className="col-center">
@@ -49,7 +78,7 @@ export default class MyProjects extends React.PureComponent<{
           </a>
         </p>
         <p>
-        When I was developing a support tool using React at my work, I found that implementing pagination with the component of Bootstrap requires a particular effort. Unfortunately, there's no difference with Reactstrap either. So, I developed a higher level of pagination component for Reactstrap and published it as an npm package.
+          When I was developing a support tool using React at my work, I found that implementing pagination with the component of Bootstrap requires a particular effort. Unfortunately, there's no difference with Reactstrap either. So, I developed a higher level of pagination component for Reactstrap and published it as an npm package.
         </p>
         <p>
           This component assembles Reactstrap's stateless pagination components and provides stateful pagination functionality. Import the component and place it in code, then you will instantly see a pagination bar on UI.
