767e02e918b491ce2215c10ee0cb4ff004b8897c
Yas <yasu.gemini@gmail.com>
2020-04-27 18:50:56 +1200

    make fetchMock returns error responses for unexpected requests. add fetch mock reset to test code.

--- YCodeLab/ClientApp/src/components/__tests___/ContactMe.test.tsx
+++ YCodeLab/ClientApp/src/components/__tests___/ContactMe.test.tsx
@@ -1,34 +1,42 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { shallow, mount } from 'enzyme';
-import ContactMe from '../ContactMe';
-import * as store from '../../store';
-import { actions } from '../../store/Messaging';
-import configureTestStore from '../../store/configureTestStore';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { shallow, mount } from 'enzyme'
+import ContactMe from '../ContactMe'
+import * as store from '../../store'
+import { actions } from '../../store/Messaging'
+import configureTestStore from '../../store/configureTestStore'
 
 export default describe('Contact ME page', () => {
   beforeAll(() => {
+    fetchMock.mockIf(/^.+$/, async req => {
+      console.log(`Mocking an unexpected fetch from '${req.url}'.`)
+      return {
+        status: 500,
+        body: 'This is a mocked error response.',
+      }
+    })
     fetchMock.mockIf(/^\/api\/Messaging\/Messages(\?.*)?$/, async req => {
-      console.log(`Mocking fetch from '${req.url}'`);
+      console.log(`Mocking an expected fetch from '${req.url}'.`)
       return {
         status: 200,
-        body: JSON.stringify({ message: 'This is mocked success response.' }),
-      };
-    });
-  });
+        body: JSON.stringify({ message: 'This is mocked a success response.' }),
+      }
+    })
+  })
+  afterEach(fetchMock.resetMocks)
 
   it('can submit a form.', async () => {
-    const { testStore, stethoscope } = configureTestStore();
+    const { testStore, stethoscope } = configureTestStore()
 
     let wrapper = mount(
       <Provider store={testStore}>
         <ContactMe />
       </Provider>
-    );
+    )
 
     let senderName = wrapper.find('input#senderName');
-    (senderName.instance() as any)['value'] = "test";
-    senderName.simulate('change');
+    (senderName.instance() as any)['value'] = "test"
+    senderName.simulate('change')
 
     let unsubscribe = stethoscope.subscribe(
       actions.receiveSaveMessageResult({} as store.SaveMessageResult).type,
@@ -37,10 +45,10 @@ export default describe('Contact ME page', () => {
         let senderName = wrapper.find('input#senderName')
         expect(senderName.exists()).toBeFalsy()
       }
-    );
+    )
 
-    wrapper.find('button#sendButton').simulate('click');
+    wrapper.find('button#sendButton').simulate('click')
 
-    await stethoscope.listen();
+    await stethoscope.listen()
   });
 });
