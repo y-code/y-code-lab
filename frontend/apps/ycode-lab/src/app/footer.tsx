374e13458517c5e87800ec2b01f6bc3b16524668
Yas <yas.ikeda@first-watch.co.nz>
2024-11-29 11:58:25 +1300

    Revert "fix Linkedin banner on footer"
    This reverts commit 043cf7986f8a8a9da5b4f3ca06c36bf9587a001e.

--- frontend/apps/ycode-lab/src/app/footer.tsx
+++ frontend/apps/ycode-lab/src/app/footer.tsx
@@ -17,13 +17,13 @@ export function FooterContent() {
   return (
     <Container>
       <Row>
-        <Col sm={12} md={4} lg={3} xl={3}>
+        <Col sm={12} md={4} lg={3} xl={3} xxl={3}>
           <div className={'text-center ' + styles['footer-profile-img-col']}>
             <img className='profile-img' src='/assets/profile-img.jpg'/>
             <h2 className='text-light'>Yas Ikeda</h2>
           </div>
         </Col>
-        <Col sm={12} md={8} lg={9} xl={9}>
+        <Col sm={12} md={8} lg={6} xl={6} xxl={6}>
           <p>
             I'm a Software Engineer experienced in enterprise system development. I'm most skilled in web system development and enjoy video game development in my spare time.
           </p>
@@ -31,15 +31,15 @@ export function FooterContent() {
             While I love coding, I am keenly interested in designing and developing quality systems effectively and efficiently. While documentation is essential in development processes, its management and maintenance are time-consuming and thus costly for businesses. Through past experiments in enterprise projects, I have found that UML and ArchiMate are cost-effective while encouraging and aiding discussions and communications well. These days, I'm using some utilities from TOGAF to make feature design and implementation planning profitable.
           </div>
         </Col>
-        <Col sm={12} md={12} lg={12} xl={12} className='profile-content-col text-light px-3'>
-          <Row className="py-3 align-items-center">
-            <Col sm={12} md={6} lg={6} xl={6} className="p-2 text-center">
-              <div className="LI-profile-badge d-flex justify-content-center"  data-version="v1" data-size="large" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="yasuikeda">
-                <a className="LI-simple-link" href='https://nz.linkedin.com/in/yasuikeda?trk=profile-badge'></a>
+        <Col sm={12} md={12} lg={3} xl={3} className='profile-content-col text-light'>
+          <Row>
+            <Col sm={6} md={6} lg={12} xl={12} className="text-center">
+              <div className="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="yasuikeda">
+                <a className="LI-simple-link" href='https://nz.linkedin.com/in/yasuikeda?trk=profile-badge'>Yasunori Ikeda</a>
               </div>
             </Col>
-            <Col sm={12} md={6} lg={6} xl={6} className="p-2">
-                <Row className="align-items-center">
+            <Col sm={6} md={6} lg={12} xl={12}>
+                <Row style={{ paddingTop: "20px" }} className="align-items-center">
                   <Col xs={12} sm={12} md={12} className="text-center" style={{ marginBottom: "20px" }}>
                       <a id="stack-overflow-badge" href="https://stackoverflow.com/users/9195902/yas-ikeda" target="_blank">
                         <img src="https://stackoverflow.com/users/flair/9195902.png" width="208" height="58" alt="profile for Yas Ikeda at Stack Overflow, Q&amp;A for professional and enthusiast programmers" title="profile for Yas Ikeda at Stack Overflow, Q&amp;A for professional and enthusiast programmers" />
